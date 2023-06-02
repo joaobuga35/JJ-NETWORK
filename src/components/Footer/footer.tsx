d7e986d2fd6d97b2d862784304e3e3bbd8612f4f
joaobuga35 <joaobuga35@gmail.com>
2023-06-02 16:38:49 -0300

    refactor: styles about footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -2,7 +2,7 @@ import { BsGlobe2 } from "react-icons/bs";
 
 export default function Footer() {
   return (
-    <footer className="bg-blue h-14 flex items-center justify-between py-5 px-3">
+    <footer className="bg-blue h-14 flex items-center justify-between py-5 px-3 md:justify-center md:gap-4 ">
         <h2 className="text-gray-500 font-mont font-medium">Todos os direitos reservados.</h2>
         <BsGlobe2 className="text-white-200 h-6 w-6"/>
     </footer>
